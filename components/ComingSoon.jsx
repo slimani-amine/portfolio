d6a9e5748f07f466f03afd6377126ab02fd0a3bb
slimani-amine <slimanimedamine24@gmail.com>
2024-06-26 16:48:24 +0100

    FIX:Coming soon component

--- components/ComingSoon.jsx
+++ components/ComingSoon.jsx
@@ -1,13 +1,23 @@
+"use client"
+import { motion } from "framer-motion";
+
 const ComingSoon = () => {
   return (
-    <div className="min-h-[70vh] flex flex-col items-center justify-center">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 1.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[70vh] flex flex-col items-center justify-center"
+    >
       <h1 className="text-5xl text-white font-bold mb-8 animate-pulse">
         Coming Soon
       </h1>
       <p className="text-white text-lg mb-8">
         We&apos;re working hard to bring you something amazing. Stay tuned!
       </p>
-    </div>
+    </motion.div>
   );
 };
 
